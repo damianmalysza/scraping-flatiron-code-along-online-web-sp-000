055ffcd319d98d49daf41f6a6254b5c565c353fb
Damian Malysza <damian.malysza@gmail.com>
2020-11-06 02:23:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,8 +26,8 @@ class Scraper
   end
 
   def make_courses
-    self.get_courses.each do |course|
-      Course.new.tap |course|
+    self.get_courses.each do |course_xml|
+      Course.new.tap |course
     end
   end
 
