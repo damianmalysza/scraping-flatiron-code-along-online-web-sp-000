9eca1aa3c8d20985486312b705d67733c52f6238
Damian Malysza <damian.malysza@gmail.com>
2020-11-06 02:16:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,10 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    binding.pry
+
+    doc.css(".post").each do |post|
+
+    end
   end
 
   def get_courses
